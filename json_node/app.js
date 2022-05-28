b29a50c6323ac279580cab7e2b33849b846c9f11
Suranjan <suranjanbiswas2002@gmail.com>
2022-05-29 01:53:02 +0530

    fix: fixed the fetcher for response

--- json_node/app.js
+++ json_node/app.js
@@ -2,20 +2,20 @@ const express = require("express");
 
 app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.json({
     message: "Hello from json_node server",
   });
 });
 
 app.post("/post", (req, res) => {
-  let customJson = res.body;
+  let customJson = req.body;
 
   res.status(200).json(customJson);
 });
 
 app.post("/postForm", (req, res) => {
-  res.status(200).json(JSON.stringify(res.body));
+  res.status(200).json(JSON.stringify(req.body));
 });
 
 app.listen(3000, () => {
