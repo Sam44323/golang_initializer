85042748db0deac0417d6e7e39d0fb3aadb718b7
Suranjan <suranjanbiswas2002@gmail.com>
2022-05-29 02:01:21 +0530

    add: added webrequester fetcher for localhost in response

--- webrequests/main.go
+++ webrequests/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
+func main() {
+	fmt.Println("Initializing the web_requester!")
+	PerformGetRequest()
+}
+
+func PerformGetRequest() {
+	url := "http://localhost:3000/"
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+	fmt.Println("Status_code for the response: ", response.StatusCode)
+
+	content, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println("Content of the response: ", string(content))
 }
