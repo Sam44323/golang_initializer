74e2f852808ab30df05c95666a2cb59245b0a53b
Suranjan <suranjanbiswas2002@gmail.com>
2022-05-29 01:51:55 +0530

    add: added custom initializer for json_message

--- json_node/app.js
+++ json_node/app.js
@@ -8,6 +8,16 @@ app.get("/", (req, res) => {
   });
 });
 
+app.post("/post", (req, res) => {
+  let customJson = res.body;
+
+  res.status(200).json(customJson);
+});
+
+app.post("/postForm", (req, res) => {
+  res.status(200).json(JSON.stringify(res.body));
+});
+
 app.listen(3000, () => {
   console.log("App is listening on port 3000");
 });
