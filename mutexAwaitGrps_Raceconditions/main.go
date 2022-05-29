1af192c25d4919a05da9a51eb311e82912fea229
Suranjan <suranjanbiswas2002@gmail.com>
2022-05-29 21:16:33 +0530

    update: updated to RW mutex

--- mutexAwaitGrps_Raceconditions/main.go
+++ mutexAwaitGrps_Raceconditions/main.go
@@ -9,27 +9,29 @@ func main() {
 	fmt.Println("Test for race_conditions")
 
 	wg := &sync.WaitGroup{} // storing a wait_group pointer
-	mut := &sync.Mutex{}
+	mut := &sync.RWMutex{}
 
+	mut.RLock() // add mutex locking for reading
 	var data = []int{0}
+	mut.RUnlock()
 
 	wg.Add(3) // total_number of go-routines
 	// IIFE in golang
-	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
+	go func(wg *sync.WaitGroup, mut *sync.RWMutex) {
 		mut.Lock()
 		data = append(data, 1)
 		mut.Unlock()
 		fmt.Println("One!")
 		wg.Done()
 	}(wg, mut)
-	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
+	go func(wg *sync.WaitGroup, mut *sync.RWMutex) {
 		mut.Lock()
 		data = append(data, 2)
 		mut.Unlock()
 		fmt.Println("Two!")
 		wg.Done()
 	}(wg, mut)
-	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
+	go func(wg *sync.WaitGroup, mut *sync.RWMutex) {
 		mut.Lock()
 		data = append(data, 3)
 		mut.Unlock()
