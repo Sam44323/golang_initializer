c3cd7e09eccea63c459f831e0ed6fe0ae35503b9
Suranjan <suranjanbiswas2002@gmail.com>
2022-05-29 14:31:05 +0530

    add: added initializer for models and middlewares

--- API/main.go
+++ API/main.go
@@ -1,5 +1,28 @@
 package main
 
+// Models for course and author
+
+type Course struct {
+	CourseId    string  `json="courseid"`
+	CourseName  string  `json="coursename"`
+	CoursePrice int     `json="price"`
+	Author      *Author `json="author"`
+}
+
+type Author struct {
+	Fullname string `json="fullname"`
+	Website  string `json="fullname"`
+}
+
+// fake DB
+var courses []Course
+
+// middlewares
+
+func (c *Course) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
+
 func main() {
 
 }
