1d50e857622104f62418d02a65f529fd4ddebbbe
Suranjan <suranjanbiswas2002@gmail.com>
2022-05-29 20:49:58 +0530

    add: added customizer for wait_grou[s

--- wait_groups/main.go
+++ wait_groups/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 )
+
+var wg sync.WaitGroup // will be updated to pointers_later
 
 func main() {
 	websiteList := []string{
@@ -14,11 +17,17 @@ func main() {
 	}
 
 	for _, web := range websiteList {
-		getStatusCode(web)
+		go getStatusCode(web) // adding the routines to the waitgroup so that they can be waited on for completion and main is not exited before all routines are completed
+		wg.Add(1)             // adding the go_routines count (can be > 1)
 	}
+
+	wg.Wait() // usually is present at the end of the method and will wait for all the routines to complete
+
 }
 
 func getStatusCode(endpoint string) {
+	defer wg.Done() // will be sending the signal after all the other tasks are completed
+
 	result, err := http.Get(endpoint)
 
 	if err != nil {
