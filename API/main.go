994b5d4bd154d2ec4ec99bc4b73973923bbb8340
Suranjan <suranjanbiswas2002@gmail.com>
2022-05-29 16:18:28 +0530

    update: updated the aliasing

--- API/main.go
+++ API/main.go
@@ -21,7 +21,7 @@ import (
 type Course struct {
 	CourseId    string  `json:"courseid"`
 	CourseName  string  `json:"coursename"`
-	CoursePrice int     `json:"-"`
+	CoursePrice int     `json:"price"`
 	Author      *Author `json:"author"`
 }
 
