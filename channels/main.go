25b53d510db1be1ea192c133db7cb416846472a2
Suranjan <suranjanbiswas2002@gmail.com>
2022-05-29 22:09:30 +0530

    update: updated the structure for channels for go_routines

--- channels/main.go
+++ channels/main.go
@@ -13,13 +13,17 @@ func main() {
 	channel := make(chan int, 1) // creating a channel that handles integer (having 1 as buffer value so that it can listen to multiple values instead of flushing)
 
 	wg.Add(2)
-	go func(ch chan int, wg *sync.WaitGroup) {
+
+	// receive only go_routines for channel as <-chan
+	go func(ch <-chan int, wg *sync.WaitGroup) {
 		value, isChannelOpen := <-ch
 		fmt.Println("Receiving data from channel: ", value)
 		fmt.Println("Is channel open: ", isChannelOpen)
 		wg.Done()
 	}(channel, &wg)
-	go func(ch chan int, wg *sync.WaitGroup) {
+
+	// send only go_routines for channel as chan<-
+	go func(ch chan<- int, wg *sync.WaitGroup) {
 		fmt.Println("Sending data to the channel")
 		ch <- 10
 		ch <- 15
