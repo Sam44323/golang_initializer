d7d1c80470c50d4075ea06c2cd55219b7023b735
Suranjan <suranjanbiswas2002@gmail.com>
2022-05-29 21:14:42 +0530

    add: added mutex for race_condition resolvers

--- mutexAwaitGrps_Raceconditions/main.go
+++ mutexAwaitGrps_Raceconditions/main.go
@@ -9,26 +9,34 @@ func main() {
 	fmt.Println("Test for race_conditions")
 
 	wg := &sync.WaitGroup{} // storing a wait_group pointer
+	mut := &sync.Mutex{}
 
 	var data = []int{0}
 
 	wg.Add(3) // total_number of go-routines
 	// IIFE in golang
-	go func(wg *sync.WaitGroup) {
+	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
+		mut.Lock()
 		data = append(data, 1)
+		mut.Unlock()
 		fmt.Println("One!")
 		wg.Done()
-	}(wg)
-	go func(wg *sync.WaitGroup) {
+	}(wg, mut)
+	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
+		mut.Lock()
 		data = append(data, 2)
+		mut.Unlock()
 		fmt.Println("Two!")
 		wg.Done()
-	}(wg)
-	go func(wg *sync.WaitGroup) {
+	}(wg, mut)
+	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
+		mut.Lock()
 		data = append(data, 3)
+		mut.Unlock()
 		fmt.Println("Three!")
 		wg.Done()
-	}(wg)
+	}(wg, mut)
 
 	wg.Wait()
+	fmt.Println(data)
 }
