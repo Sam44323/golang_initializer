468fd3fde907c14e3f31bace0c61977105e91535
Suranjan <suranjanbiswas2002@gmail.com>
2022-05-29 21:08:38 +0530

    update: updated the initializer for wait_group

--- mutexAwaitGrps_Raceconditions/main.go
+++ mutexAwaitGrps_Raceconditions/main.go
@@ -12,24 +12,22 @@ func main() {
 
 	var data = []int{0}
 
+	wg.Add(3) // total_number of go-routines
 	// IIFE in golang
 	go func(wg *sync.WaitGroup) {
-		defer wg.Done()
-
 		data = append(data, 1)
 		fmt.Println("One!")
+		wg.Done()
 	}(wg)
 	go func(wg *sync.WaitGroup) {
-		defer wg.Done()
-
 		data = append(data, 2)
 		fmt.Println("Two!")
+		wg.Done()
 	}(wg)
 	go func(wg *sync.WaitGroup) {
-		defer wg.Done()
-
 		data = append(data, 3)
 		fmt.Println("Three!")
+		wg.Done()
 	}(wg)
 
 	wg.Wait()
