5651016848e90d18b544f6dde304823626859a31
Suranjan <suranjanbiswas2002@gmail.com>
2022-05-29 20:40:04 +0530

    add: added initializer for endpoint

---
+++ wait_groups/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+}
+
+func getStatusCode(endpoint string) {
+	result, err := http.Get(endpoint)
+
+	if err != nil {
+		fmt.Println("Oops!")
+	}
+
+	fmt.Printf("%d status code endpoint \n", result.StatusCode)
+}
